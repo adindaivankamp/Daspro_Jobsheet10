8ca66a62a667546f4535856f24c8e0772d04b122
adindaivankamp <adindaivankamaysandaputri@gmail.com>
2023-11-22 10:09:39 +0700

    6

--- ModifikasiBioskop01.java
+++ ModifikasiBioskop01.java
@@ -28,5 +28,9 @@ public class ModifikasiBioskop01 {
         for (int i = 0; i < penonton.length; i++) {
             System.out.println("Panjang Baris ke-" + (i+1) + ": " + penonton[i].length);
         }
+
+        for (String[] barisPenonton : penonton){
+            System.out.println("Panjang baris: " + barisPenonton.length);
+        }
     }
 }
